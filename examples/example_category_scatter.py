1ec756254417205c132f2eb46c4d3386a13d39ad
Andrew Straw <strawman@astraw.com>
2012-06-24 00:29:56 +0200

    initial CategoryScatter example works

---
+++ examples/example_category_scatter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import numpy as np
+import os, sys
+import strawlab_mpl.defaults as smd; smd.setup_defaults()
+from strawlab_mpl.category_scatter import CategoryScatter
+from strawlab_mpl.spines import spine_placer, auto_reduce_spine_bounds
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+def make_category_scatter_figure():
+    # Generate some fake data in 3 categories.
+    categories = [('a',np.random.randn( 15 )),
+                  ('b',np.random.randn( 16 )+0.6),
+                  ('c',np.random.randn( 20 )-0.8),
+                  ]
+
+    # Build a figure and place a single axes instance in it.
+    fig = plt.figure(figsize=(4,3))
+    ax1 = fig.add_subplot(1,1,1)
+
+    # Use our helper function to do the category scatter plot.
+    cs = CategoryScatter( ax1, categories )
+
+    # Locate the axes spines on the left and bottom.
+    spine_placer(ax1, location='left,bottom' )
+
+    # Finalize the category scatter plot (stuff that can only be done
+    # after the spines are placed.
+    cs.finalize()
+
+    # Add standard y label.
+    ax1.set_ylabel('y value (units)')
+
+    # Now, add a final few touchups.
+    ax1.spines['bottom'].set_color('none') # don't draw bottom spine
+    ax1.yaxis.set_major_locator( mticker.MaxNLocator(nbins=4) )
+    auto_reduce_spine_bounds( ax1 )
+
+    fig.tight_layout()
+    return fig
+
+if __name__=='__main__':
+    np.random.seed(3)
+    fig = make_category_scatter_figure()
+    if 0:
+        plt.show()
+    if 1:
+        # Saving as .svg will allow opening and editing in Inkscape.
+        fname = 'scat.svg'
+        fig.savefig(fname)
+        print 'saved',fname
---
+++ strawlab_mpl/spines.py
@@ -0,0 +1,45 @@
+def spine_placer( ax, location='left,bottom' ):
+    expected_locations = ['left','right','top','bottom']
+
+    locations = location.split(',')
+    for loc in locations: # loc in ['left','right','top','bottom']
+        if loc not in expected_locations:
+            raise ValueError('unexpected spine location: %s'%loc)
+
+    for loc, spine in ax.spines.items(): #loc in ['left','right','top','bottom']
+        if loc not in expected_locations:
+            raise ValueError('unexpected spine location: %s'%loc)
+        if loc in locations:
+            spine.set_position( ('outward',10) ) # outward by 10 points
+        else:
+            spine.set_color('none') # don't draw spine
+
+    if ('left' in locations) and (not 'right' in locations):
+        ax.yaxis.set_ticks_position('left')
+    elif ('right' in locations) and (not 'left' in locations):
+        ax.yaxis.set_ticks_position('right')
+
+    if ('bottom' in locations) and (not 'top' in locations):
+        ax.xaxis.set_ticks_position('bottom')
+    elif ('top' in locations) and (not 'bottom' in locations):
+        ax.xaxis.set_ticks_position('top')
+
+def auto_reduce_spine_bounds( ax ):
+    yticks = ax.get_yticks()
+    y0, y1 = ax.viewLim.intervaly
+    ymin = min(y0,y1)
+    ymax = max(y0,y1)
+    good_cond = (yticks >= ymin) & (yticks <= ymax)
+    yticks = yticks[good_cond]
+    ax.spines['left'].set_bounds( yticks[0], yticks[-1] )
+    ax.spines['right'].set_bounds( yticks[0], yticks[-1] )
+
+
+    xticks = ax.get_xticks()
+    x0, x1 = ax.viewLim.intervalx
+    xmin = min(x0,x1)
+    xmax = max(x0,x1)
+    good_cond = (xticks >= xmin) & (xticks <= xmax)
+    xticks = xticks[good_cond]
+    ax.spines['top'].set_bounds( xticks[0], xticks[-1] )
+    ax.spines['bottom'].set_bounds( xticks[0], xticks[-1] )
