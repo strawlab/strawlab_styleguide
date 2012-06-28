d77fbfefeef579bec1ccff317a53870d2e7db5b6
Andrew Straw <strawman@astraw.com>
2012-06-28 09:26:00 +0200

    add 'bad' example of category scatter plot

--- docs/plotting.rst
+++ docs/plotting.rst
@@ -7,6 +7,11 @@ Tufte's works are great inspiration here.
 
 There is no completely automated way to make such plots, but the
 **strawlab_mpl** package contains various utilities that make this
-easier when using Matplotlib.
+easier when using `Matplotlib <http://matplotlib.sourceforge.net>`_.
 
 .. image:: images/panel_scatter.png
+
+Let's take the plot above as an example. This shows the distribution
+of samples from three categories. A simple plot done with Matplotlib would look like this.
+
+.. image:: images/scatter_bad.png
--- examples/figpanel_scatter.py
+++ examples/figpanel_scatter.py
@@ -8,12 +8,33 @@ from strawlab_mpl.spines import spine_placer, auto_reduce_spine_bounds
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
-def make_category_scatter_figure():
+def get_data():
     # Generate some fake data in 3 categories.
     categories = [('a',np.random.randn( 15 )),
                   ('b',np.random.randn( 16 )+0.6),
                   ('c',np.random.randn( 20 )-0.8),
                   ]
+    return categories
+
+def make_bad_figure():
+    categories = get_data()
+
+    fig = plt.figure(figsize=(4,3))
+    ticks = []
+    ticklabels = []
+    for i,(name, yvals) in enumerate(categories):
+        xvals = i + 0.5*np.random.uniform( size=yvals.shape ) - 0.25
+        plt.plot(xvals,yvals,'o')
+        ticks.append(i)
+        ticklabels.append( name )
+    ax = plt.gca()
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticklabels)
+    ax.set_ylabel('y value (units)')
+    return fig
+
+def make_category_scatter_figure():
+    categories = get_data()
 
     # Build a figure and place a single axes instance in it.
     fig = plt.figure(figsize=(4,3))
@@ -41,6 +62,13 @@ def make_category_scatter_figure():
     return fig
 
 if __name__=='__main__':
+    if 1:
+        np.random.seed(3) # ensure that example always looks the same
+        bad_fig = make_bad_figure()
+        fname = 'scatter_bad.png'
+        bad_fig.savefig(fname)
+        print 'saved',fname
+
     np.random.seed(3) # ensure that example always looks the same
     fig = make_category_scatter_figure()
     if 0:
